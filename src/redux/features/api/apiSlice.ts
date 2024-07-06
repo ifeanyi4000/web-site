a09405e0dbf93ae4c90ed82e08e7b662f760fa54
ifeanyi4000 <154647295+ifeanyi4000@users.noreply.github.com>
2024-07-06 09:55:58 +0100

    apiSlice.ts

--- src/redux/features/api/apiSlice.ts
+++ src/redux/features/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { userLoggedIn } from '../auth/authSlice';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api-web-g8tm.onrender.com/api/v1/",
+    baseUrl: "https://api-web-iqv2.onrender.com/api/v1/",
   }),
   endpoints: (builder) => ({
     loadUser: builder.query({
